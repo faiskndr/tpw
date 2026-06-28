7c6eea28d0ec5143bf9e52fd515db0eccb541c16
mazanv <mazayanova91@gmail.com>
2026-06-28 21:40:42 +0700

    feat: add user seeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            UserSeeder::class,
             DestinationSeeder::class,
             TourPackageSeeder::class,
             PackageImageSeeder::class,
