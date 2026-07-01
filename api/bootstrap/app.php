d61fad0b1b35902932efc9de71922b97dc7d0136
faiskndr <faisaliskandar204@gmail.com>
2026-07-01 22:51:54 +0700

    feat: middleware to ensure return json response

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(\App\Http\Middleware\ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
